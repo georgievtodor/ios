01fa9593080faa03b1a3384a84b47fca0036574e
Todor <todorgeorgiev96@gmail.com>
2017-03-23 07:00:46 -0700

    inital setup

---
+++ TV Show Calendar/Pods/Target Support Files/Pods-TV Show Calendar/Pods-TV Show Calendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TV_Show_Calendar : NSObject
+@end
+@implementation PodsDummy_Pods_TV_Show_Calendar
+@end
---
+++ TV Show Calendar/TV Show Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Show Calendar
+//
+//  Created by Todor on 3/23/17.
+//  Copyright © 2017 Todor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
