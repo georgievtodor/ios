e5e515c7038058eea3e78d3f725f340d268ea5c7
Todor <todorgeorgiev96@gmail.com>
2017-03-29 05:08:16 -0700

    dependency injection setup

--- TV Show Calendar/TV Show Calendar/DiConfig.swift
+++ TV Show Calendar/TV Show Calendar/DiConfig.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017 Todor. All rights reserved.
 //
 
-import Foundation
+import Swinject
+import SwinjectStoryboard
+
+class DiConfigHttp {
+    public static func setup(container: Container) {
+        container.register(HttpRequesterBase.self) { _ in HttpRequester() }
+    }
+}
+
+extension SwinjectStoryboard {
+    public static func setup() {
+        DiConfigHttp.setup(container: defaultContainer)
+    }
+}
