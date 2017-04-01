1a5624d27c61e84ab440bd419110ed6e248d1c77
Todor <todorgeorgiev96@gmail.com>
2017-03-31 18:27:09 -0700

    App constants

---
+++ TV Show Calendar/TV Show Calendar/Constants/ApiConstants.swift
@@ -0,0 +1,6 @@
+public final class ApiConstants {
+    private static let baseUrl: String = "https://tvshow-server.herokuapp.com"
+    public static let loginUrl: String =  "\(baseUrl)/auth/login/"
+    public static let registerUrl: String = "\(baseUrl)/auth/register/"
+    
+}
---
+++ TV Show Calendar/TV Show Calendar/Constants/TheMovieDbConstants.swift
@@ -0,0 +1,27 @@
+public final class TheMovieDbConstants {
+    private static let tMDBApiKey: String = "?api_key=f0da25f6492c7f3f3c1b40fa29de8f6c"
+    private static let tMDBApiUrl: String = "https://api.themoviedb.org/3";
+    private static let tvShowsBaseUrl: String = "\(tMDBApiUrl)/tv"
+    public static let popularTvShows: String = "\(tMDBApiUrl)/tv/popular\(tMDBApiKey)"
+    public static let imageUrl: String = "https://image.tmdb.org/t/p/w640"
+    
+    
+    func getSearchTvShowUrl(queryString query: String) -> String {
+        return "\(TheMovieDbConstants.tMDBApiUrl)/search/tv\(TheMovieDbConstants.tMDBApiKey)&language=en-US&query=\(query)&page=1"
+    }
+    
+    func getTvDetailsUrl(serialId id: String) -> String {
+        return "\(TheMovieDbConstants.tvShowsBaseUrl)/\(id)\(TheMovieDbConstants.tMDBApiKey)"
+    }
+    
+    func getSeasonDetailsUrl(serialId id: String, _ seasonNumber: Int) -> String {
+        return "\(TheMovieDbConstants.tvShowsBaseUrl)/\(id)/season/\(seasonNumber)\(TheMovieDbConstants.tMDBApiKey)"
+    }
+    
+    func getEpisodeDetailsUrl(serialId id: String, _ seasonNumber: Int, _ episodeNumber: Int) -> String {
+        return "\(TheMovieDbConstants.tvShowsBaseUrl)\(id)/season/\(seasonNumber)/episode/\(episodeNumber)\(TheMovieDbConstants.tMDBApiKey)"
+    }
+    
+    
+    
+}
